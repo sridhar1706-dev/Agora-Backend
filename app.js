b6ad0fd55818c24845d36e18f564bad68420e67b
Sridhar <naiksridhar143@gmail.com>
2026-03-28 12:37:46 +0530

    cors

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ const app = express();
 
 connectDB(); // ✅ ADD THIS
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://myagoraapp.duckdns.org",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
